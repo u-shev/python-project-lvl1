057df509e40960ba9fb7a1e77137c0629908db3a
u-shev <sheveleva.jul@gmail.com>
2022-08-10 15:22:33 +0300

    step 6  Changes to be committed: 	new file:   brain_games/games/calc_game.py 	modified:   brain_games/run_game1.py 	modified:   brain_games/scripts/brain_calc.py 	modified:   README.md 	deleted:    brain_games/games/calc.py 	modified:   dist/hexlet-code-0.1.0.tar.gz 	modified:   dist/hexlet_code-0.1.0-py3-none-any.whl

--- brain_games/run_game1.py
+++ brain_games/run_game1.py
@@ -6,9 +6,9 @@ def run_game(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
-    print('What is the result of the expression?')
+    print(game.MAIN_QUESTION)
     for _ in range(3):
-        question, correct_answer = game()
+        question, correct_answer = game.tasks_inputs()
         print('Question: {}'.format(question))
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,11 +3,12 @@
 
 
 from brain_games.run_game1 import run_game
-from brain_games.games.calc import calc
+from brain_games.games import calc_game
 
-
+""""run calc game"""
 def main():
-    run_game(calc)
+    run_game(calc_game)
+
 
 
 if __name__ == '__main__':
