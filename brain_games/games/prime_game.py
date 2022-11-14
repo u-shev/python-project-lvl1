acc69d7e44e844d504d9f4cf6615e5e221521af4
u-shev <sheveleva.jul@gmail.com>
2022-11-14 20:07:12 +0300

    prime game 	new file:   ../games/prime_game.py 	new file:   brain_prime.py 	modified:   ../../README.md 	modified:   ../../dist/hexlet-code-0.1.0.tar.gz 	modified:   ../../dist/hexlet_code-0.1.0-py3-none-any.whl 	modified:   ../../pyproject.toml

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,21 @@
+from random import randint
+
+MAIN_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    """Check if number is prime or not."""
+    if number < 2 or not number % 2:
+        return False
+    counter = 3
+    while counter <= number // 2:
+        if not number % counter:
+            return False
+        counter += 2
+    return True
+
+def tasks_inputs():
+    number = randint(1, 100)
+    question = f'{number}'
+    correct_answer = 'yes' if is_prime(number) else 'no'
+    return (question,correct_answer)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Prime game"""
+
+from brain_games.run_game1 import run_game
+from brain_games.games import prime_game
+
+""""run prime  game"""
+def main():
+    run_game(prime_game)
+
+
+
+if __name__ == '__main__':
+    main()
