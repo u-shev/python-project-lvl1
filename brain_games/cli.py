3a8c52829749307a499912cfe1e5b8fba5553fab
u-shev <sheveleva.jul@gmail.com>
2022-11-17 19:34:04 +0300

    modified:   cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,4 +1,4 @@
 def welcome_user():
     import prompt
-    name = prompt.string('May I have your name?')
-    print('Hello,' + name + '!')
+    name = prompt.string('May I have your name? ')
+    print('Hello,' + name + ' !')
