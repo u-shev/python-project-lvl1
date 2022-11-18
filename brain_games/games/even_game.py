4729c1913b315f528ce4d71e487b9e97ab03e9b9
u-shev <sheveleva.jul@gmail.com>
2022-11-18 15:55:39 +0300

    fix even game 	new file:   even_game.py 	modified:   ../scripts/brain_even.py

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,16 @@
+"""special questions for even game"""
+
+from random import randint
+
+MAIN_QUESTION = 'Answer "yes" if number even otherwise answer "no".'
+
+def tasks_inputs():
+    random_number = randint(1,100)
+    question = f'{random_number}'
+    if random_number % 2 == 0: 
+        correct_answer = 'yes' 
+    if random_number % 2 > 0:
+        correct_answer = 'no'
+    return question, correct_answer
+
+    
