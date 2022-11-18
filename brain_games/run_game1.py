8debb7ba1eef2785bcf0d3a1765c551732bffdcc
u-shev <sheveleva.jul@gmail.com>
2022-11-18 19:07:47 +0300

    fix linter 	modified:   run_game1.py

--- brain_games/run_game1.py
+++ brain_games/run_game1.py
@@ -15,10 +15,8 @@ def run_game(game):
         if answer == correct_answer:
             print('Correct!')
         else:
-            print(
-                f'"{answer}" is wrong answer ;(.'
-                f'Correct answer was "{correct_answer}".'
-                 )
+            print(f'"{answer}" is wrong answer ;(.'
+                f'Correct answer was "{correct_answer}".')
             print(f"Let's try again, {name}!")
             return
     print('Congratulations, {}!'.format(name))
